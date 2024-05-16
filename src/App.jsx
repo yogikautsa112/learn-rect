9ab55ae1eec1efbc472c025827a62add6dd68dd8
yogikautsa112 <yogikautsaralnandeta@smkwikrama.sch.id>
2024-05-16 12:36:47 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./Header"
-import TodoList from "./TodoList.jsx"
+import Header from "./components/Header"
+import TodoList from "./components/TodoList.jsx"
 
 function App() {
   return (
